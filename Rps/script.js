24eb7de921a88babcd1f1bb5721520ccc0ecc909
cedogithub <39746523+cedogithub@users.noreply.github.com>
2022-10-12 14:09:29 -0400

    Add files via upload
    added the UI to the game

--- Rps/script.js
+++ Rps/script.js
@@ -1,8 +1,52 @@
-//global variables for the game 
+//global variables for the game
 let choice = ["rock", "paper", "scissor"];
 let playerScore = 0;
 let cpuScore = 0;
-let round = 0
+let round = 0;
+let button = document.querySelectorAll("button");
+let divResults = document.querySelector(".round-results");
+let playerSelection ='';
+
+
+//displaying the rounds results in the html
+function para(test){
+  const p = document.createElement("p");
+  p.textContent = test;
+  divResults.appendChild(p);
+}
+function rematch(){
+  let rematchBtn = document.createElement("button");
+  rematchBtn.textContent = "rematch";
+  divResults.appendChild(rematchBtn);
+  rematchBtn.addEventListener("click", () =>{
+    divResults.innerHTML = "";
+    playerScore = 0;
+    cpuScore = 0;
+    round =0;
+    for (let i = 0; i < button.length; i++) {
+      button[i].disabled = false;
+    }
+  })
+  
+}
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", (e) => {
+    switch (e.target.dataset.id) {
+      case "rock": 
+      playerSelection = "rock"
+      break;
+      case "paper": 
+      playerSelection = "paper"
+      break;
+      case "scissor": 
+      playerSelection = "scissor"
+
+    }
+    
+    game();
+  })
+}
 
 //computer choice of rps
 function getComputerChoice() {
@@ -11,73 +55,80 @@ function getComputerChoice() {
 }
 
 // logic to determine the winner or loser of the game
-    function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    let round = 0;
-    if (playerSelection === computerSelection) {
-        console.log("its a tie!");
-        return "No winner"
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let round = 0;
+  if (playerSelection === computerSelection) {
+    para("its a tie!");
+    return "No winner";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    para("You lost! paper beats rock!");
+    return "Cpu";
+  } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    para("You lost ! rocks beats scissor!");
+    return "Cpu";
+  } else if (playerSelection === "paper" && computerSelection === "scissor") {
+    para("You lost ! scissor beats paper!");
+    return "Cpu";
+  } else {
+    para("You win " + playerSelection + " beats " + computerSelection);
+    return "Player";
+  }
+}
+//stats for every round of the game
+function statsRound(user) {
+  let theWinner = "";
+  if (user.includes("Player")) {
+    round++;
+    playerScore++;
+    para("Round " + round + " winner is: " + user);
+    para("Player Score: " + playerScore)
+    para("Cpu Score: " + cpuScore);
+  } else if (user.includes("Cpu")) {
+    round++;
+    cpuScore++;
+    para("Round " + round + " winner is " + user);
+    para("Cpu Score: " + cpuScore);
+    para("Player Score: " + playerScore)
+  } else if (user.includes("No winner")) {
+    round++;
+    para("Round " + round);
+    para("Player Score: " + playerScore)
+    para("Cpu Score: " + cpuScore);
+  }
+}
 
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log( "You lost! paper beats rock!");
-        return "Cpu"
 
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
-        console.log( "You lost ! rocks beats scissor!");
-        return "Cpu"
+//Starts the game 
+function game() {
+    let computerSelection = getComputerChoice();
 
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-        console.log("You lost ! scissor beats paper!");
-        return "Cpu"
-
-    } else {
-        console.log("You win "+ playerSelection + " beats " + computerSelection);
-        return "Player"
+    if (
+      playerSelection === "scissor" ||
+      playerSelection === "rock" ||
+      playerSelection === "paper"
+    ) {      
+      divResults.innerHTML =''
+      let winner = playRound(playerSelection, computerSelection);
+      statsRound(winner);
     }
-    }
+  if (playerScore ==5) {
+    para("The winner of the game is you!");
+    
+for (let i = 0; i < button.length; i++) {
+  button[i].disabled = true;
+}
+rematch();
+}
 
-//Starts the game with 5 rounds and announces winner at the end of the game
-    function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt(
-        "Choose Rock , Paper or scissor"
-        ).toLowerCase();
-        let computerSelection = getComputerChoice();
 
-        if (playerSelection === "scissor" || playerSelection  === "rock"  || playerSelection === "paper") {
-            let winner = (playRound(playerSelection, computerSelection));
-            statsRound(winner);
-        }
-        
-    }
-    if(playerScore>cpuScore){
-        console.log('The winner of the game is you!')
-    }
-    if(cpuScore > playerScore ){
-        console.log('The winner of the game is the cpu!')
-    }
-    }
-
-    //stats for every round of the game
-    function statsRound(user){
-        let theWinner ='';
-        if(user.includes("Player")){
-            round++;
-            playerScore++;
-            console.log("Round " + round)
-            console.log("Player Score:" + playerScore + " Cpu Score:" + cpuScore );
-        }
-        else if(user.includes("Cpu")){
-            round++;
-            cpuScore++;
-            console.log("Round " + round)
-            console.log("Cpu Score:" + cpuScore + " Player Score:" + playerScore);
-        }
-        else if(user.includes("No winner")){
-            round++;
-            console.log("Round " + round)
-        }
+  if (cpuScore == 5) {
+    para("The winner of the game is the cpu!");
+    for (let i = 0; i < button.length; i++) {
+      button[i].disabled = true;
 
     }
+    rematch();
+  }
+}
 
-console.log(game());
